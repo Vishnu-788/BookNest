6d58265e6886651264ab2334a1c2338db82d287f
Vishnu Prasad <prasadvishnu788@gmail.com>
2025-08-24 19:49:45 +0530

    Migrating to Angular frontend.

--- client/src/components/reader/BookDetail/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BookDetail: React.FC = () => {
-  return <div>BookDetail</div>;
-};
-
-export default BookDetail;
--- client/src/utils/api.ts
+++
@@ -1,8 +0,0 @@
-export const BASE_URL = "http://127.0.0.1:8000/api/v1";
-
-export const API_ENDPOINTS = {
-  SIGN_IN: `${BASE_URL}/auth/login`,
-  SIGN_UP: `${BASE_URL}/auth/register`,
-  BOOK_CREATE: `${BASE_URL}/books/create`,
-  BOOKS: `${BASE_URL}/books`,
-};
